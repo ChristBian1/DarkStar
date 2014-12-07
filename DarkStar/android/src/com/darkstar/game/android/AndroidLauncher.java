9ef281ed2551604b0d3440461485c445d3fcce4f
Roberto Ruffolo <robertoruffolo@hotmail.com>
2014-12-06 19:32:03 -0500

    By the love of God

---
+++ DarkStar/android/src/com/darkstar/game/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.darkstar.game.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.darkstar.game.DarkStar;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new DarkStar(), config);
+	}
+}
---
+++ DarkStar/desktop/src/com/darkstar/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.darkstar.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.darkstar.game.DarkStar;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DarkStar(), config);
+	}
+}
